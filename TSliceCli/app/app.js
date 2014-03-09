340330c61d46e4f634698477c82007e0ed70a2d0
xsatter <anders.satter@gmail.com>
2014-03-09 08:26:17 +0100

    first implementation of ui-router

--- TSliceCli/app/app.js
+++ TSliceCli/app/app.js
@@ -14,43 +14,49 @@ angular.module('testApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+   'ui.router'
 
 ])
-  .config(function ($routeProvider) {
-    // We use AngularJS dependency injection to fetch the route provider.
-    // The route provider is used to setup our app's routes. 
- 
-    // The config below simply says when you visit '/' it'll render
-    // the views/main.html template controlled by the MainCtrl controller.
- 
-    // The otherwise method specifies what the app should do if it doesn't recognise 
-    // the route entered by a user. In this case, redirect to home
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/myRoute', {
-        templateUrl: 'views/myroute.html',
-        controller: 'MyrouteCtrl'
-      })
-      .when('/rest1', {
-        templateUrl: 'views/rest1.html',
-        controller: 'Rest1Ctrl'
-      })
-      .when('/timeslicer/allItems', {
-        templateUrl: 'reporting/allitems.view.html',
-        controller: 'TimeslicerCtrl'
-      })
-      .when('/test/promise', {
-        templateUrl: 'scripts/test/promise.view.html',
-        controller: 'PromiseCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+//  .config(function ($routeProvider) {
+//    // We use AngularJS dependency injection to fetch the route provider.
+//    // The route provider is used to setup our app's routes.
+//
+//    // The config below simply says when you visit '/' it'll render
+//    // the views/main.html template controlled by the MainCtrl controller.
+//
+//    // The otherwise method specifies what the app should do if it doesn't recognise
+//    // the route entered by a user. In this case, redirect to home
+//    $routeProvider
+//      .when('/', {
+//        templateUrl: 'views/main.html',
+//        controller: 'MainCtrl'
+//      })
+//      .when('/myRoute', {
+//        templateUrl: 'views/myroute.html',
+//        controller: 'MyrouteCtrl'
+//      })
+//      .when('/rest1', {
+//        templateUrl: 'views/rest1.html',
+//        controller: 'Rest1Ctrl'
+//      })
+//      .when('/timeslicer/allItems', {
+//        templateUrl: 'reporting/allitems.view.html',
+//        controller: 'TimeslicerCtrl'
+//      })
+//      .when('/test/promise', {
+//        templateUrl: 'scripts/test/promise.view.html',
+//        controller: 'PromiseCtrl'
+//      })
+//      .otherwise({
+//        redirectTo: '/'
+//      });
+//  });
+
+
+
+
+
   angular.module('testApp').config(function($httpProvider){
     $httpProvider.interceptors.push('myHttpInterceptor');
   });
---
+++ TSliceCli/app/ui-state.js
@@ -0,0 +1,38 @@
+angular.module('testApp')
+  .config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /timeslicer/allItems
+    $urlRouterProvider.otherwise("/timeslicer/allItems");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('allItems', {
+        url: '/timeslicer/allItems',
+        templateUrl: 'reporting/allitems.view.html',
+        controller: 'TimeslicerCtrl'
+      })
+      .state('allItems.list', {
+        url: '/test/promise',
+        templateUrl: 'scripts/test/promise.view.html',
+        controller: 'PromiseCtrl'
+      })
+
+//      .state('state1.list', {
+//        url: "/list",
+//        templateUrl: "partials/state1.list.html",
+//        controller: function($scope) {
+//          $scope.items = ["A", "List", "Of", "Items"];
+//        }
+//      })
+//      .state('state2', {
+//        url: "/state2",
+//        templateUrl: "partials/state2.html"
+//      })
+//      .state('state2.list', {
+//        url: "/list",
+//        templateUrl: "partials/state2.list.html",
+//        controller: function($scope) {
+//          $scope.things = ["A", "Set", "Of", "Things"];
+//        }
+//      })
+  });
